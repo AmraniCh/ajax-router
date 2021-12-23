9f9dbf718c7cb7617fbf2ddfb569367ac73029db
AmraniCh <contact@amranich.dev>
2021-12-23 10:19:16 +0100

    Added Handler::fromFile

---
+++ src/Exception/HandlersFileException.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace AmraniCh\AjaxDispatcher\Exception;
+
+/**
+ * AmraniCh\AjaxDispatcher\Exception\HandlersFileException
+ *
+ * @since  1.0.0
+ * @author El Amrani Chakir <contact@amranich.dev>
+ * @link   https://amranich.dev
+ */
+class HandlersFileException extends AjaxDispatcherException
+{
+
+}
