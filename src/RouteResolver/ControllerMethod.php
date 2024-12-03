d65f2f19f3019d2be8c37d1701710958a6054220
Shakir El Amrani <elamrani.sv.laza@gmail.com>
2024-12-03 12:05:16 +0100

    [ControllerMethod] removed forgotten var_dump

--- src/RouteResolver/ControllerMethod.php
+++ src/RouteResolver/ControllerMethod.php
@@ -93,8 +93,6 @@ class ControllerMethod
             );
         }
 
-       //var_dump(array_values($args));exit;
-
         $callable = [
             is_string($this->class) ? new $this->class() : $this->class,
             $this->name
